49099d6f85e2fb2becf79cb3c9fa63a9dc817140
monzey <maxi.bertrand@gmail.com>
2016-03-24 20:38:02 +0100

    clean index routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         let users = this.store.findAll('user');
-
-        let user = this.store.findRecord('user', 65);
-
-        // console.log(users.getEach('username'));
-
         return users;
     }
 });
