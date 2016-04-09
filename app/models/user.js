6bc69f6be6bd73fe6ecff7f16bb927f49ba1e62a
Maxime Bertrand <maxi.bertrand@gmail.com>
2016-04-09 15:36:04 +0200

    added profiles model to users

--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,14 @@
 import DS from 'ember-data';
-import App from '../app';
+import App from 'ember-application';
 
 App.UserModel = DS.Model.extend({
     username: DS.attr('string'),
     email: DS.attr('string'),
     contactInformations: DS.belongsTo('contact-information', {
+        async: false,
+        inverse: null
+    }),
+    profiles: DS.hasMany('profile', {
         async: false,
         inverse: null
     })
