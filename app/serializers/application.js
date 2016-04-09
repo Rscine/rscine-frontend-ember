a693c2476a192b9f701bd8683efc4e95a24bafcd
Maxime Bertrand <maxi.bertrand@gmail.com>
2016-04-09 15:34:18 +0200

    managed to make serialization for embeded resources work !!

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,23 +1,49 @@
 import DS from 'ember-data';
+import App from 'ember-application';
 
-export default DS.RESTSerializer.extend({
+App.ApplicationSerializer =  DS.RESTSerializer.extend({
 
     /**
      * @{inheritdoc}
      */
     normalizeResponse (store, primaryModelClass, payload, id, requestType) {
-        var normalizedPayload = {};
+        let normalizedPayload = {};
         normalizedPayload[primaryModelClass.modelName] = payload;
-        console.log(normalizedPayload);
-        return this._super(store, primaryModelClass, normalizedPayload, id, requestType)
+        // extraction des relations embedded
+        normalizedPayload[primaryModelClass.modelName] = this._extractEmbeddedRelationships(normalizedPayload[primaryModelClass.modelName]);
+        // extraction des relations _links
+        // normalizedPayload = this._extractLinkedRelationships(payload);
+        return this._super(store, primaryModelClass, normalizedPayload, id, requestType);
     },
 
-    normalizeFindBelongsToResponse (store, primaryModelClass, payload, id, requestType) {
-        console.log('hey');
+    /**
+     * @desc Extrait les relations ancrées dans _embedded (HATEOAS)
+     * @param  {Object} payload
+     * @return {Object}
+     */
+    _extractEmbeddedRelationships (originalPayload) {
+        for (let resource of originalPayload) {
+            for (var relationship in resource._embedded) {
+                resource[relationship] = resource._embedded[relationship];
+            }
+        }
+        return originalPayload;
     },
 
-    keyForLink (key, kind) {
-        return this._super('_links', kind);
-    }
+    /**
+     * @desc Extrait les relations ancrées dans _links (HATEOAS)
+     * @param  {Object} payload
+     * @return {Object}
+     */
+    // _extractLinkedRelationships (payload) {
+    //     for (var resource of payload) {
+    //         for (var relationship in resource._links) {
+    //             resource[relationship] = resource._links[relationship];
+    //         }
+    //     }
+    //     return payload;
+    // }
 
 });
+
+export default App.ApplicationSerializer;
