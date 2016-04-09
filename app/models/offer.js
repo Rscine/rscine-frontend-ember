237de17fa47e41abf580083d10c47c9b8a45180f
Maxime Bertrand <maxi.bertrand@gmail.com>
2016-04-09 20:16:09 +0200

    activate offers handler, creator and applicants

--- app/models/offer.js
+++ app/models/offer.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     name: DS.attr('string'),
     description: DS.attr('string'),
-    // creator: DS.belongsTo(),
-    // applicants: DS.hasMany(),
-    // handler: DS.belongsTo()
+    creator: DS.belongsTo('user', {async: true}),
+    applicants: DS.hasMany('user', {async: true}),
+    handler: DS.belongsTo('user', {async: true})
 });
---
+++ app/serializers/offer.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+import ApplicationSerializer from '../serializers/application';
+
+export default ApplicationSerializer.extend({
+
+});
