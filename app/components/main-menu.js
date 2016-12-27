685e45003443a35bfb043ee8a5e6561b2e90f73b
Maxime Bertrand <maxi.bertrand@gmail.com>
2016-12-27 17:55:30 +0100

    maj du menu pour le passer à gauche

--- app/components/main-menu.js
+++ app/components/main-menu.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        openSideBar() {
+            Ember.$('.ui.sidebar')
+              .sidebar('toggle')
+            ;
+        }
+    }
 });
